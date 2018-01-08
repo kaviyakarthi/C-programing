ee2dcf3f4dce4ce9db7f1af43265df72291b133b
kaviyakarthi <32099909+kaviyakarthi@users.noreply.github.com>
2018-01-08 16:53:13 +0530

    Create positive.C

---
+++ positive.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a=3;
+    if(a>0)
+    {
+        printf("POSITIVE ");
+        
+    }
+    else{
+        printf("NEGATIVE / ZERO");
+    }
+    return 0;
+}
