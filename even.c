0b876d768409fa1bd505b84a4b77945b1a03570a
kaviyakarthi <32099909+kaviyakarthi@users.noreply.github.com>
2018-01-19 19:35:44 +0530

    Rename 15.c to even.c

---
+++ even.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main()
+
+{
+     int i,m =6,n=9;
+
+   for (i = m+1; i<= n; i++)
+
+        {
+
+               if (i% 2 == 0)
+
+                  printf ("%d ", i);
+
+         }
+
+                return 0;
+
+}
