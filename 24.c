86c7cbb85a62cb0abd0eadf5eb7b483226420aa8
kaviyakarthi <32099909+kaviyakarthi@users.noreply.github.com>
2018-01-21 04:44:13 +0000

    Create 24.c

---
+++ 24.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+	int a[100]={2,1,3}; 
+	int i,j,n=3,temp;
+for(i=0;i<n;i++)
+{
+	for(j=i+1;j<n;j++)
+	{
+		if(a[i]>a[j])
+		{
+		temp=a[i];
+		a[i]=a[j];
+		a[j]=temp;
+		}
+	}
+}
+for(i=0;i<n;i++)
+{
+	printf("%d\t",a[i]);
+}
+
+	
+	
+	return 0;
+}
