d4d0d4a555ab2325b6d4bf04dbb1232e9a95c5f0
kaviyakarthi <32099909+kaviyakarthi@users.noreply.github.com>
2018-01-29 19:20:28 +0530

    Create 45.c

---
+++ 45.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	int a=123,count=0,temp;
+	while(a!=0);
+	{
+		temp=a/10;
+		temp=a;
+		count=++count;
+	}
+	
+	printf("The no.of digits are:%d",count);
+	
+	return 0;
+}
