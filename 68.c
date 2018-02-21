0e19c3e83a2a0c6933326ad39472466a6e1a0c3e
kaviyakarthi <32099909+kaviyakarthi@users.noreply.github.com>
2018-02-21 16:46:36 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  printf("Enter the number ");
+  scanf("%d",&n);
+  if(n%7==0)
+  printf("Multiples of 7");
+  else
+  printf("Not multiplies of 7");
+  return 0;
+}
