2ffb8a90b1e35a957b72c5970933c116fd206fce
kaviyakarthi <32099909+kaviyakarthi@users.noreply.github.com>
2018-02-28 16:31:22 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n,a,b;
+	printf("enter num & limits");
+	scanf("%d %d %d",&n,&a,&b);
+	if((n>'a')&&(n<'b'))
+	printf("\nYes lies between %d and %d",a,b);
+	else
+	printf("\nNo");
+	return 0;
+
+
+	return 0;
+}
